4c03dbc8bf39a25659407e0570d11d9df3f77d1c
BrainClouder <celso.w.dias.a@gmail.com>
2020-05-04 23:52:35 -0300

    test

---
+++ build/precache-manifest.fb2e2843528a47f27b4318e2ae9c59ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d3eb10ad9aff9e8fba0fb75d65aa68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0a82e588af9fe7f0e2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c6fa69c5a0eccd63e029",
+    "url": "/static/js/2.5e21d1b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e21d1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0a82e588af9fe7f0e2",
+    "url": "/static/js/main.a05069bc.chunk.js"
+  },
+  {
+    "revision": "f57f8c7f033975353c41",
+    "url": "/static/js/runtime-main.26de8da9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
